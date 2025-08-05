a002d59bd33d8e7b0d94aff56834ad0d3bfbbd63
jadebenn <jadebenn@users.noreply.github.com>
2025-08-05 06:58:38 -0500

    Use default rustfmt settings (#1)
    * tabs to spaces
    
    * allow reordering
    
    * enable line wrapping
    
    * run rustfmt on lu_packets_derive

--- examples/capture_parser/zip_context.rs
+++ examples/capture_parser/zip_context.rs
@@ -3,125 +3,134 @@ use std::io::Result as Res;
 
 use endio_bit::BEBitReader;
 use lu_packets::{
-	lu,
-	raknet::client::replica::{
-		ComponentConstruction, ComponentSerialization, ReplicaContext,
-		achievement_vendor::{AchievementVendorConstruction, AchievementVendorSerialization},
-		base_combat_ai::{BaseCombatAiConstruction, BaseCombatAiSerialization},
-		bbb::{BbbConstruction, BbbSerialization},
-		bouncer::{BouncerConstruction, BouncerSerialization},
-		buff::BuffConstruction,
-		character::{CharacterConstruction, CharacterSerialization},
-		collectible::{CollectibleConstruction, CollectibleSerialization},
-		controllable_physics::{ControllablePhysicsConstruction, ControllablePhysicsSerialization},
-		donation_vendor::{DonationVendorConstruction, DonationVendorSerialization},
-		destroyable::{DestroyableConstruction, DestroyableSerialization},
-		fx::FxConstruction,
-		inventory::{InventoryConstruction, InventorySerialization},
-		item::{ItemConstruction, ItemSerialization},
-		level_progression::{LevelProgressionConstruction, LevelProgressionSerialization},
-		lup_exhibit::{LupExhibitConstruction, LupExhibitSerialization},
-		module_assembly::ModuleAssemblyConstruction,
-		moving_platform::{MovingPlatformConstruction, MovingPlatformSerialization},
-		mutable_model_behavior::{MutableModelBehaviorConstruction, MutableModelBehaviorSerialization},
-		phantom_physics::{PhantomPhysicsConstruction, PhantomPhysicsSerialization},
-		player_forced_movement::{PlayerForcedMovementConstruction, PlayerForcedMovementSerialization},
-		pet::{PetConstruction, PetSerialization},
-		possessable::{PossessableConstruction, PossessableSerialization},
-		possession_control::{PossessionControlConstruction, PossessionControlSerialization},
-		quickbuild::{QuickbuildConstruction, QuickbuildSerialization},
-		racing_control::{RacingControlConstruction, RacingControlSerialization},
-		rigid_body_phantom_physics::{RigidBodyPhantomPhysicsConstruction, RigidBodyPhantomPhysicsSerialization},
-		script::ScriptConstruction,
-		scripted_activity::{ScriptedActivityConstruction, ScriptedActivitySerialization},
-		shooting_gallery::{ShootingGalleryConstruction, ShootingGallerySerialization},
-		simple_physics::{SimplePhysicsConstruction, SimplePhysicsSerialization},
-		skill::SkillConstruction,
-		switch::{SwitchConstruction, SwitchSerialization},
-		vehicle_physics::{VehiclePhysicsConstruction, VehiclePhysicsSerialization},
-		vendor::{VendorConstruction, VendorSerialization},
-	},
-	world::{Lot, LuNameValue, LnvValue},
+    lu,
+    raknet::client::replica::{
+        ComponentConstruction, ComponentSerialization, ReplicaContext,
+        achievement_vendor::{AchievementVendorConstruction, AchievementVendorSerialization},
+        base_combat_ai::{BaseCombatAiConstruction, BaseCombatAiSerialization},
+        bbb::{BbbConstruction, BbbSerialization},
+        bouncer::{BouncerConstruction, BouncerSerialization},
+        buff::BuffConstruction,
+        character::{CharacterConstruction, CharacterSerialization},
+        collectible::{CollectibleConstruction, CollectibleSerialization},
+        controllable_physics::{ControllablePhysicsConstruction, ControllablePhysicsSerialization},
+        destroyable::{DestroyableConstruction, DestroyableSerialization},
+        donation_vendor::{DonationVendorConstruction, DonationVendorSerialization},
+        fx::FxConstruction,
+        inventory::{InventoryConstruction, InventorySerialization},
+        item::{ItemConstruction, ItemSerialization},
+        level_progression::{LevelProgressionConstruction, LevelProgressionSerialization},
+        lup_exhibit::{LupExhibitConstruction, LupExhibitSerialization},
+        module_assembly::ModuleAssemblyConstruction,
+        moving_platform::{MovingPlatformConstruction, MovingPlatformSerialization},
+        mutable_model_behavior::{
+            MutableModelBehaviorConstruction, MutableModelBehaviorSerialization,
+        },
+        pet::{PetConstruction, PetSerialization},
+        phantom_physics::{PhantomPhysicsConstruction, PhantomPhysicsSerialization},
+        player_forced_movement::{
+            PlayerForcedMovementConstruction, PlayerForcedMovementSerialization,
+        },
+        possessable::{PossessableConstruction, PossessableSerialization},
+        possession_control::{PossessionControlConstruction, PossessionControlSerialization},
+        quickbuild::{QuickbuildConstruction, QuickbuildSerialization},
+        racing_control::{RacingControlConstruction, RacingControlSerialization},
+        rigid_body_phantom_physics::{
+            RigidBodyPhantomPhysicsConstruction, RigidBodyPhantomPhysicsSerialization,
+        },
+        script::ScriptConstruction,
+        scripted_activity::{ScriptedActivityConstruction, ScriptedActivitySerialization},
+        shooting_gallery::{ShootingGalleryConstruction, ShootingGallerySerialization},
+        simple_physics::{SimplePhysicsConstruction, SimplePhysicsSerialization},
+        skill::SkillConstruction,
+        switch::{SwitchConstruction, SwitchSerialization},
+        vehicle_physics::{VehiclePhysicsConstruction, VehiclePhysicsSerialization},
+        vendor::{VendorConstruction, VendorSerialization},
+    },
+    world::{LnvValue, Lot, LuNameValue},
 };
 use zip::read::ZipFile;
 
 use super::Cdclient;
 
-const COMP_ORDER: [u32; 35] = [108, 61, 1, 30, 20, 3, 40, 98, 7, 110, 109, 106, 4, 26, 17, 5, 9, 60, 11, 48, 25, 16, 100, 102, 19, 39, 23, 75, 42, 6, 49, 2, 44, 71, 107];
+const COMP_ORDER: [u32; 35] = [
+    108, 61, 1, 30, 20, 3, 40, 98, 7, 110, 109, 106, 4, 26, 17, 5, 9, 60, 11, 48, 25, 16, 100, 102,
+    19, 39, 23, 75, 42, 6, 49, 2, 44, 71, 107,
+];
 
 pub struct ZipContext<'a> {
-	pub zip: ZipFile<'a>,
-	pub comps: &'a mut HashMap<u16, Vec<u32>>,
-	pub cdclient: &'a mut Cdclient,
-	pub assert_fully_read: bool,
+    pub zip: ZipFile<'a>,
+    pub comps: &'a mut HashMap<u16, Vec<u32>>,
+    pub cdclient: &'a mut Cdclient,
+    pub assert_fully_read: bool,
 }
 
 impl ZipContext<'_> {
-	fn apply_whitelist(comps: &mut Vec<u32>, config: &Option<LuNameValue>) {
-		if let Some(conf) = config {
-			if let Some(LnvValue::I32(1)) = conf.get(&lu!("componentWhitelist")) {
-				comps.retain(|&x| match x {
-					1 | 2 | 3 | 7 | 10 | 11 | 24 | 42 => true,
-					_ => false,
-				});
-			}
-		}
-	}
+    fn apply_whitelist(comps: &mut Vec<u32>, config: &Option<LuNameValue>) {
+        if let Some(conf) = config {
+            if let Some(LnvValue::I32(1)) = conf.get(&lu!("componentWhitelist")) {
+                comps.retain(|&x| match x {
+                    1 | 2 | 3 | 7 | 10 | 11 | 24 | 42 => true,
+                    _ => false,
+                });
+            }
+        }
+    }
 
-	fn apply_config_overrides(comps: &mut Vec<u32>, config: &Option<LuNameValue>) {
-		if comps.contains(&42) {
-			if let Some(conf) = config {
-				if conf.contains_key(&lu!("modelBehaviors")) {
-					if let Some(LnvValue::I32(m_type)) = conf.get(&lu!("modelType")) {
-						let new_phys = if *m_type == 0 { 1 } else { 3 };
-						if let Some(phys_index) = comps.iter().position(|&x| x == 1 || x == 3) {
-							comps[phys_index] = new_phys;
-						} else {
-							comps.push(new_phys);
-						}
-						return;
-					}
-				}
-			}
-			if !comps.iter().any(|&x| x == 1 || x == 3) {
-				comps.push(3);
-			}
-		}
-	}
+    fn apply_config_overrides(comps: &mut Vec<u32>, config: &Option<LuNameValue>) {
+        if comps.contains(&42) {
+            if let Some(conf) = config {
+                if conf.contains_key(&lu!("modelBehaviors")) {
+                    if let Some(LnvValue::I32(m_type)) = conf.get(&lu!("modelType")) {
+                        let new_phys = if *m_type == 0 { 1 } else { 3 };
+                        if let Some(phys_index) = comps.iter().position(|&x| x == 1 || x == 3) {
+                            comps[phys_index] = new_phys;
+                        } else {
+                            comps.push(new_phys);
+                        }
+                        return;
+                    }
+                }
+            }
+            if !comps.iter().any(|&x| x == 1 || x == 3) {
+                comps.push(3);
+            }
+        }
+    }
 
-	fn apply_component_overrides(comps: &Vec<u32>, final_comps: &mut Vec<u32>) {
-		for comp in comps {
-			// special case: utter bodge
-			match comp {
-				2 => {
-					final_comps.push(44);
-				}
-				4 => {
-					final_comps.push(110);
-					final_comps.push(109);
-					final_comps.push(106);
-				}
-				7 => {
-					final_comps.push(98);
-				}
-				23 | 48 => {
-					if !final_comps.contains(&7) {
-						final_comps.push(7);
-					}
-				}
-				_ => {}
-			}
-			final_comps.push(*comp);
-		}
-		// special case: utter bodge
-		if final_comps.contains(&26) {
-			final_comps.remove(final_comps.iter().position(|&x| x == 11).unwrap());
-			final_comps.remove(final_comps.iter().position(|&x| x == 42).unwrap());
-		}
-	}
+    fn apply_component_overrides(comps: &Vec<u32>, final_comps: &mut Vec<u32>) {
+        for comp in comps {
+            // special case: utter bodge
+            match comp {
+                2 => {
+                    final_comps.push(44);
+                }
+                4 => {
+                    final_comps.push(110);
+                    final_comps.push(109);
+                    final_comps.push(106);
+                }
+                7 => {
+                    final_comps.push(98);
+                }
+                23 | 48 => {
+                    if !final_comps.contains(&7) {
+                        final_comps.push(7);
+                    }
+                }
+                _ => {}
+            }
+            final_comps.push(*comp);
+        }
+        // special case: utter bodge
+        if final_comps.contains(&26) {
+            final_comps.remove(final_comps.iter().position(|&x| x == 11).unwrap());
+            final_comps.remove(final_comps.iter().position(|&x| x == 42).unwrap());
+        }
+    }
 
 	#[rustfmt::skip]
-	fn map_constrs<R: std::io::Read>(comps: &Vec<u32>) -> Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentConstruction>>> {
+    fn map_constrs<R: std::io::Read>(comps: &Vec<u32>) -> Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentConstruction>>> {
 		use endio::Deserialize;
 
 		let mut constrs: Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentConstruction>>> = vec![];
@@ -170,31 +179,36 @@ impl ZipContext<'_> {
 }
 
 impl std::io::Read for ZipContext<'_> {
-	fn read(&mut self, buf: &mut [u8]) -> Res<usize> {
-		self.zip.read(buf)
-	}
+    fn read(&mut self, buf: &mut [u8]) -> Res<usize> {
+        self.zip.read(buf)
+    }
 }
 
 // hacky hardcoded components to be able to read player replicas without DB lookup
 impl ReplicaContext for ZipContext<'_> {
-	fn get_comp_constructions<R: std::io::Read>(&mut self, network_id: u16, lot: Lot, config: &Option<LuNameValue>) -> Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentConstruction>>> {
-		let mut comps = self.cdclient.get_comps(lot).clone();
+    fn get_comp_constructions<R: std::io::Read>(
+        &mut self,
+        network_id: u16,
+        lot: Lot,
+        config: &Option<LuNameValue>,
+    ) -> Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentConstruction>>> {
+        let mut comps = self.cdclient.get_comps(lot).clone();
 
-		Self::apply_whitelist(&mut comps, config);
-		Self::apply_config_overrides(&mut comps, config);
+        Self::apply_whitelist(&mut comps, config);
+        Self::apply_config_overrides(&mut comps, config);
 
-		comps.sort_by_key(|x| COMP_ORDER.iter().position(|y| y == x).unwrap_or(usize::MAX));
-		comps.dedup();
+        comps.sort_by_key(|x| COMP_ORDER.iter().position(|y| y == x).unwrap_or(usize::MAX));
+        comps.dedup();
 
-		let mut final_comps = vec![];
-		Self::apply_component_overrides(&comps, &mut final_comps);
-		let constrs = Self::map_constrs(&final_comps);
-		self.comps.insert(network_id, final_comps);
-		constrs
-	}
+        let mut final_comps = vec![];
+        Self::apply_component_overrides(&comps, &mut final_comps);
+        let constrs = Self::map_constrs(&final_comps);
+        self.comps.insert(network_id, final_comps);
+        constrs
+    }
 
 	#[rustfmt::skip]
-	fn get_comp_serializations<R: std::io::Read>(&mut self, network_id: u16) -> Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentSerialization>>> {
+    fn get_comp_serializations<R: std::io::Read>(&mut self, network_id: u16) -> Vec<fn(&mut BEBitReader<R>) -> Res<Box<dyn ComponentSerialization>>> {
 		use endio::Deserialize;
 
 		if let Some(comps) = self.comps.get(&network_id) {
