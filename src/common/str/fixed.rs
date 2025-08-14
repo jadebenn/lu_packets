9ea32cee7785d3bf1e1e2e93f22a44a2ee6757d3
jadebenn <jadebenn@users.noreply.github.com>
2025-08-13 20:18:04 -0500

    trying desperately to make const generics work for strings

--- src/common/str/fixed.rs
+++ src/common/str/fixed.rs
@@ -1,6 +1,4 @@
-use std::convert::TryFrom;
-use std::io::Result as Res;
-use std::io::{Read, Write};
+use std::{convert::TryFrom, io::{Read, Result as Res, Write}, ops::Not};
 
 use endio::{Deserialize, LE, Serialize};
 
@@ -8,128 +6,113 @@ use super::{AbstractLuStr, AsciiChar, AsciiError, LuChar, LuStrExt, Ucs2Char, Uc
 
 // todo[const generics]: const generic strings
 // todo: exclude the final null terminator from the array
-macro_rules! abstract_lu_str {
-    ($name:ident, $c:ty, $null:expr, $n:literal) => {
-        // todo: runtime type invariants checks (valid, null terminator)
-        /// A string with a maximum length of $n.
-        pub struct $name([$c; $n]);
 
-        impl PartialEq for $name {
-            fn eq(&self, other: &Self) -> bool {
-                dbg!((&**self) == (&**other))
-            }
-        }
+// todo: runtime type invariants checks (valid, null terminator)
+/// A string with a maximum length of $n.
+pub struct LuString<C, const N: usize>([C; N]);
 
-        impl std::fmt::Debug for $name {
-            fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-                (&**self).fmt(f)
-            }
-        }
-
-        impl std::ops::Deref for $name {
-            type Target = AbstractLuStr<$c>;
-
-            #[inline]
-            fn deref(&self) -> &Self::Target {
-                let terminator = self.0.iter().position(|&c| c == $null).unwrap();
-                &self.0[..terminator]
-            }
-        }
-
-        impl std::ops::DerefMut for $name {
-            #[inline]
-            fn deref_mut(&mut self) -> &mut Self::Target {
-                let terminator = self.0.iter().position(|&c| c == $null).unwrap();
-                &mut self.0[..terminator]
-            }
-        }
-
-        impl<R: Read> Deserialize<LE, R> for $name {
-            fn deserialize(reader: &mut R) -> Res<Self> {
-                let mut bytes = [0u8; $n * std::mem::size_of::<$c>()];
-                reader.read(&mut bytes)?;
-                Ok(Self(unsafe { std::mem::transmute(bytes) }))
-            }
-        }
-
-        impl<W: Write> Serialize<LE, W> for &$name {
-            fn serialize(self, writer: &mut W) -> Res<()> {
-                let x: [u8; $n * std::mem::size_of::<$c>()] =
-                    unsafe { std::mem::transmute(self.0) };
-                writer.write_all(&x)
-            }
-        }
-    };
+impl<C: PartialEq, const N: usize> PartialEq for LuString<C, N> {
+    fn eq(&self, other: &Self) -> bool {
+        dbg!((&**self) == (&**other))
+    }
 }
 
-macro_rules! lu_str {
-    ($name:ident, $n:literal) => {
-        abstract_lu_str!($name, AsciiChar, AsciiChar(0), $n);
-
-        impl TryFrom<&[u8]> for $name {
-            type Error = AsciiError;
-
-            fn try_from(string: &[u8]) -> Result<Self, Self::Error> {
-                if string.len() >= $n {
-                    // actually length error but whatever
-                    return Err(AsciiError);
-                }
-                let mut bytes = [0u8; $n];
-                // todo: ascii range check
-                for (i, chr) in string.iter().enumerate() {
-                    bytes[i] = *chr;
-                }
-                let bytes = unsafe { std::mem::transmute(bytes) };
-                Ok(Self(bytes))
-            }
-        }
-
-        impl<const N: usize> TryFrom<&[u8; N]> for $name {
-            type Error = AsciiError;
-
-            fn try_from(string: &[u8; N]) -> Result<Self, Self::Error> {
-                Self::try_from(&string[..])
-            }
-        }
-    };
+impl<C, const N: usize> std::fmt::Debug for LuString<C, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        (&**self).fmt(f)
+    }
 }
 
-macro_rules! lu_wstr {
-    ($name:ident, $n:literal) => {
-        abstract_lu_str!($name, Ucs2Char, Ucs2Char(0), $n);
+impl<C: Copy + Not<Output = bool>, const N: usize> std::ops::Deref for LuString<C, N> {
+    type Target = AbstractLuStr<C>;
 
-        impl TryFrom<&str> for $name {
-            type Error = Ucs2Error;
-
-            fn try_from(string: &str) -> Result<Self, Self::Error> {
-                let mut bytes = [0u16; $n];
-                for (i, chr) in string.encode_utf16().take($n - 1).enumerate() {
-                    bytes[i] = chr;
-                }
-                let bytes = unsafe { std::mem::transmute(bytes) };
-                Ok(Self(bytes))
-            }
-        }
-
-        impl From<&$name> for String {
-            fn from(wstr: &$name) -> Self {
-                String::from_utf16(unsafe {
-                    &*(&**wstr as *const [Ucs2Char] as *const [<Ucs2Char as LuChar>::Int])
-                })
-                .unwrap()
-            }
-        }
-    };
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        let terminator = self.0.iter().position(|&c| !c).unwrap();
+        &self.0[..terminator]
+    }
 }
 
-lu_str!(LuString3, 3);
-lu_str!(LuString33, 33);
-lu_str!(LuString37, 37);
-lu_wstr!(LuWString32, 32);
-lu_wstr!(LuWString33, 33);
-lu_wstr!(LuWString41, 41);
-lu_wstr!(LuWString42, 42);
-lu_wstr!(LuWString50, 50);
-lu_wstr!(LuWString128, 128);
-lu_wstr!(LuWString256, 256);
-lu_wstr!(LuWString400, 400);
+impl<C: Copy + Not<Output = bool>, const N: usize> std::ops::DerefMut for LuString<C, N> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        let terminator = self.0.iter().position(|&c| !c).unwrap();
+        &mut self.0[..terminator]
+    }
+}
+
+impl<R: Read, C, const N: usize> Deserialize<LE, R> for LuString<C, N> {
+    fn deserialize(reader: &mut R) -> Res<Self> {
+        let mut bytes = [0u8; N * std::mem::size_of::<C>()];
+        reader.read(&mut bytes)?;
+        Ok(Self(unsafe { std::mem::transmute(bytes) }))
+    }
+}
+
+impl<W: Write, C, const N: usize> Serialize<LE, W> for &LuString<C, N> {
+    fn serialize(self, writer: &mut W) -> Res<()> {
+        let x: [u8; _] =
+            unsafe { std::mem::transmute(self.0) };
+        writer.write_all(&x)
+    }
+}
+
+impl<C, const N: usize> TryFrom<&[u8]> for LuString<C, N> {
+    type Error = AsciiError;
+
+    fn try_from(string: &[u8]) -> Result<Self, Self::Error> {
+        if string.len() >= N {
+            // actually length error but whatever
+            return Err(AsciiError);
+        }
+        let mut bytes = [0u8; N];
+        // todo: ascii range check
+        for (i, chr) in string.iter().enumerate() {
+            bytes[i] = *chr;
+        }
+        let bytes = unsafe { std::mem::transmute(bytes) };
+        Ok(Self(bytes))
+    }
+}
+
+impl<C, const N: usize> TryFrom<&[u8; N]> for LuString<C, N> {
+    type Error = AsciiError;
+
+    fn try_from(string: &[u8; N]) -> Result<Self, Self::Error> {
+        Self::try_from(&string[..])
+    }
+}
+
+impl<C, const N: usize> TryFrom<&str> for LuString<C, N> {
+    type Error = Ucs2Error;
+
+    fn try_from(string: &str) -> Result<Self, Self::Error> {
+        let mut bytes = [0u16; N];
+        for (i, chr) in string.encode_utf16().take(N - 1).enumerate() {
+            bytes[i] = chr;
+        }
+        let bytes = unsafe { std::mem::transmute(bytes) };
+        Ok(Self(bytes))
+    }
+}
+
+impl<C, const N: usize> From<&LuString<C, N>> for String {
+    fn from(wstr: &LuString<C, N>) -> Self {
+        String::from_utf16(unsafe {
+            &*(&**wstr as *const [Ucs2Char] as *const [<Ucs2Char as LuChar>::Int])
+        })
+        .unwrap()
+    }
+}
+
+pub type LuString3 = LuString<u8, 3>;
+pub type LuString33 = LuString<u8, 33>;
+pub type LuString37 = LuString<u8, 37>;
+pub type LuWString32 = LuString<u16, 32>;
+pub type LuWString33 = LuString<u16, 33>;
+pub type LuWString41 = LuString<u16, 41>;
+pub type LuWString42 = LuString<u16, 42>;
+pub type LuWString50 = LuString<u16, 50>;
+pub type LuWString128 = LuString<u16, 128>;
+pub type LuWString256 = LuString<u16, 256>;
+pub type LuWString400 = LuString<u16, 400>;
